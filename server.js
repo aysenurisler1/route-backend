ba7811131af397afaf6e234afa04f53b1c784cc1
nehirmartin2132 <nehirmartin@gmail.com>
2026-07-13 18:47:43 +0300

    Nodemailer IPv4 baglantisina zorlandi (Render IPv6 sorunu duzeltmesi)

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const { Pool } = require("pg");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
